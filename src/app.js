d9f2c619a9fb24858c43f2c62b15a5e5023d307b
kittybagpipes <sparticus978@gmail.com>
2020-11-30 16:18:02 -0800

    added get route

--- src/app.js
+++ src/app.js
@@ -13,10 +13,36 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-const addresses = [];
+const addresses = [
+  {
+    id: 348345384353483453483453,
+    firstName: "Bradford",
+    lastName: "Bossen",
+    address1: "1234 Thirst Dr.",
+    address2: "1234 Thirst Dr.",
+    city: "Alarada",
+    state: "CA",
+    zip: "92333",
+  },
+  {
+    id: 348345384353483453483453,
+    firstName: "Bradford",
+    lastName: "Bossen",
+    address1: "1234 Thirst Dr.",
+    address2: "1234 Thirst Dr.",
+    city: "Alarada",
+    state: "CA",
+    zip: "92333",
+  },
+];
 
 app.get("/address", (req, res) => {
-  res.send("Hello, boilerplate; BRUV");
+  //if (addresses.length === 0) {
+  //  return res.status(400).send("NO ADDRESS DATA PRESENT, PLEASE ADD");
+  //} else {
+  //return
+  res.json(addresses);
+  // }
 });
 
 app.use(function errorHandler(error, req, res, next) {
--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,26 @@
+const { expect } = require("chai");
+const supertest = require("supertest");
 const app = require("../src/app");
 
 describe("App", () => {
-  it('GET / responds with 200 containing "Hello, world! BRUV"', () => {
-    return supertest(app).get("/").expect(200, "Hello, boilerplate; BRUV");
+  it("GET /address responds with 200 AND an array when data is present", () => {
+    return supertest(app)
+      .get("/address")
+      .expect(200)
+      .expect("Content-type", "application/json; charset=utf-8")
+      .then((res) => {
+        expect(res.body).to.be.an("array");
+        const app = res.body[0];
+        expect(app).to.include.all.keys(
+          "id",
+          "firstName",
+          "lastName",
+          "address1",
+          "address2",
+          "city",
+          "state",
+          "zip"
+        );
+      });
   });
 });
